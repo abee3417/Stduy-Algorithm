c170785b19099274fa148a70fd700f3a66a2f223
Yunseong Yang <abee3417@naver.com>
2023-09-08 13:40:03 +0900

    Add question 11729

---
+++ Baekjoon/재귀/11729.py
@@ -0,0 +1,11 @@
+def hanoi(n, src, des, aux):
+    if (n == 1):
+        print(src + " " + des)
+    else:
+        hanoi(n-1, src, aux, des)
+        print(src + " " + des)
+        hanoi(n-1, aux, des, src)
+
+n = int(input())
+print(2**n - 1)
+hanoi(n, '1', '3', '2')
