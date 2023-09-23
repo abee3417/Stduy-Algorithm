9e6fae7ad8353f72942e1d7a5a2b77ed2adaa5a4
Yunseong Yang <abee3417@naver.com>
2023-09-23 09:56:56 +0900

    Add question 15657

---
+++ Baekjoon/백트래킹/15657.py
@@ -0,0 +1,8 @@
+from itertools import combinations_with_replacement
+n, m = map(int, input().split())
+li = sorted(list(map(int, input().split())))
+ans = sorted(list(combinations_with_replacement(li, m)))
+for i in ans:
+    for j in i:
+        print(j, end=" ")
+    print()
