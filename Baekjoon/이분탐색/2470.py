654ac2753ffdd95681b10fca6180a198ed9b3672
Yunseong Yang <abee3417@naver.com>
2023-11-16 00:01:53 +0900

    Add question 2470

---
+++ Baekjoon/이분탐색/2470.py
@@ -0,0 +1,13 @@
+#2467과 동일문제
+N = int(input())
+liq = sorted(list(map(int, input().split())))
+l, r = 0, -1
+std = 2000000001
+ans = [liq[l], liq[r]]
+while (liq[l] < liq[r]):
+    if (abs(liq[l]+liq[r]) <= std):
+        std = abs(liq[l]+liq[r])
+        ans = [liq[l], liq[r]]
+    if (liq[l]+liq[r] > 0): r -= 1
+    else: l += 1
+print(ans[0], ans[1])
